f92c2e515a3ecc95f58ecafa99912993060779c3
Rui Pires <pires.rui@outlook.pt>
2022-12-27 13:51:03 +0000

    nginx and gunicorn

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,13 @@
 
 sudo yum install python3 pip3
 sudo yum install python3-devel
+sudo yum install nginx
 sudo yum install postgresql
+
 sudo pip3 install flask
 sudo pip3 install psycopg2
 sudo pip3 install pexpect
+sudo pip3 install gunicorn
 
+
+./netconfig
--- server.py
+++ server.py
@@ -56,4 +56,4 @@ def default_callback(e = None):
     return "default_callback", 200
 
 app.register_error_handler(Exception, default_callback)
-app.run()
+#app.run()
