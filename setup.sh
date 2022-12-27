3b695456f024fe734b064d9602adfc26aeffaee1
Rui Pires <rui@localhost.localdomain>
2022-12-27 01:09:30 +0000

    certificates and stuff

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo yum install python3 pip3
+sudo yum install python3-devel
+sudo yum install postgresql
+sudo pip3 install flask
+sudo pip3 install psycopg2
+sudo pip3 install pexpect
+
