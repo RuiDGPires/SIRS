3c5ddda6d1ea704946351af0d75419cfc1c5448d
Rui Pires <pires.rui@outlook.pt>
2022-12-29 15:34:37 +0000

    Fix setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo yum install python3 pip3 -y
+sudo yum update -y
+sudo yum install python3 python3-pip3 -y
 sudo yum install python3-devel -y
 sudo yum install nginx -y
 sudo yum install tmux -y
@@ -11,5 +12,7 @@ sudo pip3 install psycopg2
 sudo pip3 install pexpect
 sudo pip3 install gunicorn
 
+readonly CWD=$(dirname "$0")
+readonly FILE=$CWD/../ifcfg-enp0s8 
 
-./netconfig
+$CWD/netconfig $FILE
