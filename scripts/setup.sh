15570adf00bbca21d3f02404d34f1bdf61b30c1d
Rui Pires <pires.rui@outlook.pt>
2022-12-29 15:43:05 +0000

    Fix setup stuff

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-sudo yum update -y
-sudo yum install python3 python3-pip3 -y
-sudo yum install python3-devel -y
-sudo yum install nginx -y
-sudo yum install tmux -y
-sudo yum install postgresql -y
+if [ ! "$1" = "--skip-install" ]; then
+	sudo yum update -y
+	sudo yum install python3 python3-pip3 -y
+	sudo yum install python3-devel -y
+	sudo yum install nginx -y
+	sudo yum install tmux -y
+	sudo yum install postgresql -y
 
-sudo pip3 install flask
-sudo pip3 install psycopg2
-sudo pip3 install pexpect
-sudo pip3 install gunicorn
+	sudo pip3 install flask
+	sudo pip3 install psycopg2
+	sudo pip3 install pexpect
+	sudo pip3 install gunicorn
+fi
 
 readonly CWD=$(dirname "$0")
-readonly FILE=$CWD/../ifcfg-enp0s8 
+readonly NETWORKFILE=$CWD/../ifcfg-enp0s8 
+readonly NGINXFILE=$CWD/../nginx.conf
 
-$CWD/netconfig $FILE
+$CWD/netconfig $NETWORKFILE
+$CWD/nginx-configure $NGINXFILE
