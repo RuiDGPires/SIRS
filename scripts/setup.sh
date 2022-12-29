959906aa5b03fd96b08beeb5172034cd8a3c058b
Rui Pires <pires.rui@outlook.pt>
2022-12-29 16:41:19 +0000

    add scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! "$1" = "--skip-install" ]; then
+	sudo yum update -y
+	sudo yum install python3 python3-pip3 -y
+	sudo yum install tmux -y
+	sudo yum install postgresql -y
+fi
+
+readonly CWD=$(dirname "$0")
+readonly NETWORKFILE=$CWD/../ifcfg-enp0s8 
+
+$CWD/netconfig $NETWORKFILE
