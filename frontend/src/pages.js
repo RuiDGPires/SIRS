94989ecbb283bfecdb3d0d163f9ffba2302bf656
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2022-12-30 17:48:34 +0000

    Customer Login Page

--- frontend/src/pages.js
+++ frontend/src/pages.js
@@ -1,3 +1,11 @@
 function login_employee() {
     console.log(document.getElementById("employee_user").value);
+    // TODO -> Send request to server to login?
+    window.location.href = "./employee.html"
+}
+
+function login_customer() {
+    console.log(document.getElementById("customer_user").value);
+    // TODO -> Send request to server to login?
+    window.location.href = "./customer.html"
 }
