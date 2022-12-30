d56b8d6766d9234363c3be44b0428e1775fbb67d
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2022-12-30 18:00:06 +0000

    Add username to pages

--- frontend/src/pages.js
+++ frontend/src/pages.js
@@ -1,11 +1,27 @@
 function login_employee() {
-    console.log(document.getElementById("employee_user").value);
+    employee = document.getElementById("employee_user").value
+    console.log(employee);
     // TODO -> Send request to server to login?
-    window.location.href = "./employee.html"
+    window.location.href = "./employee.html?user=" + employee
 }
 
 function login_customer() {
-    console.log(document.getElementById("customer_user").value);
+    customer = document.getElementById("customer_user").value
+    console.log(customer);
     // TODO -> Send request to server to login?
-    window.location.href = "./customer.html"
+    window.location.href = "./customer.html?user=" + customer
+}
+
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
 }
