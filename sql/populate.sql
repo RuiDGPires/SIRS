2658a1816487e0fd5ffbc6b2d5a42bf60db1d38a
Rui Pires <pires.rui@outlook.pt>
2023-01-05 03:41:03 +0000

    bikes

--- sql/populate.sql
+++ sql/populate.sql
@@ -1,6 +1,9 @@
 INSERT INTO users (secret) VALUES ('TVBRW2DTRY35QXJRQKZAIEX3PKUID5O3');
 INSERT INTO clients (id, name) VALUES ((select MAX(id) FROM users), 'Rui');
 
+INSERT INTO users (secret) VALUES ('P7ZI2QTVTSPX6HPAQEU3HXFNBH6RCBLB');
+INSERT INTO employees (id, name) VALUES ((select MAX(id) FROM users), 'Rita');
+
 
 INSERT INTO bikes (latitude, longitude, rightly_parked) VALUES (
     38.737784,
