f7917b5037d66d769dce59cd9aaffc65e9ab5f04
Rui Pires <55214955+RuiDGPires@users.noreply.github.com>
2023-01-06 12:28:03 +0000

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,12 +4,15 @@ if [ ! "$1" = "--skip-install" ]; then
 	sudo yum update -y
 	sudo yum install python3 python3-pip3 -y
 	sudo yum install python3-devel -y
+	sudo yum install postgresql postgresql-devel -y
+	sudo yum install epel-release -y
 	sudo yum install nginx -y
 	sudo yum install tmux -y
-	sudo yum install postgresql -y
+	
 
 	sudo pip3 install flask
 	sudo pip3 install pyotp
+	sudo pip3 install psycopg2-binary
 	sudo pip3 install psycopg2
 	sudo pip3 install pexpect
 	sudo pip3 install pyjwt
