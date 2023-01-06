791486da54c0d97482aa30e06c6944a19c826c9a
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-01-06 02:47:23 -0500

    Fix

--- frontend/src/pages/edit-customer.jsx
+++ frontend/src/pages/edit-customer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { updateCustomerCall } from "../service/service";
